36ecaad4679f758a026ad6db23ee04431760fb78
Charlotte Brazier <charlottebrazier@hotmail.com>
2023-08-05 14:26:42 +0100

    changed timeblock div id locations to point to relevant section in local storage, edited function logic for event listener on save button

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,8 @@
 // DOM elements to link to JQuery
 var displayDateHeader = $("#currentDay");
 var timeSegment = $(".timeBlock");
-var plannerEntry = $(".eventEntry");
 var timeBlocksSection = $(".timeBlocks");
 var timeNow = moment().hours();
-var buttonClicked = $(".saveBtn");
 
 // Display the current date
 function displayDate() {
@@ -12,19 +10,8 @@ function displayDate() {
   displayDateHeader.text(todayDate);
 }
 displayDate();
-//Add an event listener for the button
-plannerEntry.on("click", ".saveBtn", savePlannerEntries);
-//console.log("button clicked!");
-// Save all of the planner entries to local storage when the save button is clicked
-function savePlannerEntries() {
-  localStorage.setItem("plannerEntry", JSON.stringify(plannerEntry));
-}
-//Need to prevent default clearing of the form when the save button is clicked.
-//function protectSavedEntry(event) {
-//event.preventDefault();
-//}
 
-// Make it loop through the time divs variable to check whether the listed times are in the past, present of future
+// Make it loop through the time divs variable to check whether the listed times are in the past, present or future
 for (var i = 0; i < timeSegment.length; i++) {
   if (timeSegment[i].dataset.time < timeNow) {
     timeSegment[i].classList.remove("future");
@@ -43,5 +30,25 @@ for (var i = 0; i < timeSegment.length; i++) {
     timeSegment[i].classList.add("future");
   }
 }
-// ***** DON'T FORGET TO CALL FUNCTIONS!!!!*******
-//protectSavedEntry();
+
+//Add an event listener for the save button. It needs to pull through the ID of the time block it is contained within.
+$(".saveBtn").on("click", function () {
+  var plannerEntry = $(this).siblings(".eventEntry").val();
+  var hour = $(this).parent().attr("id");
+  localStorage.setItem(plannerEntry, hour);
+});
+
+console.log(localStorage);
+
+// Retrieve entries from local storage
+$("8am .eventEntry").val(localStorage.getItem("8am"));
+$("9am .eventEntry").val(localStorage.getItem("9am"));
+$("10am .eventEntry").val(localStorage.getItem("10am"));
+$("11am .eventEntry").val(localStorage.getItem("11am"));
+$("12pm .eventEntry").val(localStorage.getItem("12pm"));
+$("1pm .eventEntry").val(localStorage.getItem("1pm"));
+$("2pm .eventEntry").val(localStorage.getItem("2pm"));
+$("3pm .eventEntry").val(localStorage.getItem("3pm"));
+$("4pm .eventEntry").val(localStorage.getItem("4pm"));
+
+//Need to prevent default clearing of the form when the page is refreshed.
