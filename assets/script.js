1ac1d6f6ba95f113bcd0a59e47ea6ce0c0e75ffd
Charlotte Brazier <charlottebrazier@hotmail.com>
2023-08-06 13:54:53 +0100

    added id hashes and find jQuery to the local storage items so they wouldn't disappear on refresh, also changed the key and value round in the button click function

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   // DOM elements to link to JQuery
   var displayDateHeader = $("#currentDay");
   var timeSegment = $(".timeBlock");
-  //Get the current time so the calendar can compare itself to change the colours
+  // Get the current time so the calendar can compare itself to change the colours
   var timeNow = moment().hours();
 
   // Display the current date
@@ -32,26 +32,22 @@ $(document).ready(function () {
     }
   }
 
-  //Add an event listener for the save button. It needs to pull through the ID of the time block it is contained within.
-  //Need to prevent default clearing of the form when the page is refreshed.
-  $(".saveBtn").click(function (event) {
-    //console.log("The save button has been clicked!"); - This is working
-    //prompt("Excellent work! Your plan has been saved."); - This is working
-    event.preventDefault();
+  // Add an event listener for the save button. It needs to pull through the ID of the time block it is contained within.
+  // There should be no default clearing of the form when the page is refreshed.
+  $(".saveBtn").click(function () {
     var plannerEntry = $(this).siblings(".eventEntry").val();
     var hour = $(this).parent().attr("id");
-    localStorage.setItem(plannerEntry, hour);
+    localStorage.setItem(hour, plannerEntry);
   });
-  // Retrieve entries from local storage
-  $("8am .eventEntry").val(localStorage.getItem("8am"));
-  $("9am .eventEntry").val(localStorage.getItem("9am"));
-  $("10am .eventEntry").val(localStorage.getItem("10am"));
-  $("11am .eventEntry").val(localStorage.getItem("11am"));
-  $("12pm .eventEntry").val(localStorage.getItem("12pm"));
-  $("1pm .eventEntry").val(localStorage.getItem("1pm"));
-  $("2pm .eventEntry").val(localStorage.getItem("2pm"));
-  $("3pm .eventEntry").val(localStorage.getItem("3pm"));
-  $("4pm .eventEntry").val(localStorage.getItem("4pm"));
 
-  console.log(localStorage);
+  // Retrieve entries from local storage
+  $("#8am").find(".eventEntry").val(localStorage.getItem("8am"));
+  $("#9am").find(".eventEntry").val(localStorage.getItem("9am"));
+  $("#10am").find(".eventEntry").val(localStorage.getItem("10am"));
+  $("#11am").find(".eventEntry").val(localStorage.getItem("11am"));
+  $("#12pm").find(".eventEntry").val(localStorage.getItem("12pm"));
+  $("#1pm").find(".eventEntry").val(localStorage.getItem("1pm"));
+  $("#2pm").find(".eventEntry").val(localStorage.getItem("2pm"));
+  $("#3pm").find(".eventEntry").val(localStorage.getItem("3pm"));
+  $("#4pm").find(".eventEntry").val(localStorage.getItem("4pm"));
 });
